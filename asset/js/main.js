53fd5c9d6deae7b220f9cf81f9c07936ec084e13
thong <thongva@regit-technology.com>
2022-09-19 15:43:08 +0700

    create variable for color

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,12 +6,11 @@ var heightHeader = header.offsetHeight;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
 
-    if (prevScrollPos > currentScrollPos) { // Srcoll up    
+    if (prevScrollPos > currentScrollPos) { // Scroll up    
         if (currentScrollPos <= heightHeader) {
             header.classList.remove("scroll-up");
             header.classList.add("scroll-down");
-        }
-        else {
+        } else {
             header.classList.add("scroll-up");
             header.classList.remove("scroll-down");
         }
@@ -23,10 +22,10 @@ window.onscroll = function () {
 
 }
 
-/* Actice pagition when click */
-var pagitionNumbers = document.querySelectorAll('.pagination-number');
+/* Active pagination when click */
+var paginationNumbers = document.querySelectorAll('.pagination-number');
 
-pagitionNumbers.forEach(function (number) {
+paginationNumbers.forEach(function (number) {
     number.onclick = function () {
         document.querySelector('.pagination-number.active').classList.remove('active');
         this.classList.add('active');
@@ -43,8 +42,6 @@ var logoText = document.querySelector('.name-company');
 
     btnSearch.addEventListener("click", function(event){
         event.preventDefault();
-        console.log('tab search', tabSearch);
-        console.log('btnToggle', btnToggle);
         headerSearch.classList.toggle("display");
         tabSearch.classList.toggle("display");
         logoImg.classList.toggle("hide");
